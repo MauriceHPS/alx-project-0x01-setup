72919849086ecad21592bd09c51c7673aac73ccc
MauriceHPS <maurice@headpoint.info>
2026-01-06 05:50:29 +0300

    Renamed the variable that retrieves data from the API. user->posts without optionality (?)

--- alx-project-0x01/.next/dev/static/chunks/[root-of-the-server]__983d34e1._.js
+++ alx-project-0x01/.next/dev/static/chunks/[root-of-the-server]__983d34e1._.js
@@ -902,7 +902,7 @@ const Users = ({ posts })=>{
                         }, ("TURBOPACK compile-time value", void 0)),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$alx$2d$project$2d$0x01$2d$setup$2f$alx$2d$project$2d$0x01$2f$node_modules$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                             className: "grid grid-cols-3 gap-2",
-                            children: posts?.map(({ id, name, username, email, address, phone, website, company }, key)=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$alx$2d$project$2d$0x01$2d$setup$2f$alx$2d$project$2d$0x01$2f$node_modules$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$alx$2d$project$2d$0x01$2d$setup$2f$alx$2d$project$2d$0x01$2f$components$2f$common$2f$UserCard$2e$tsx__$5b$client$5d$__$28$ecmascript$29$__["default"], {
+                            children: posts.map(({ id, name, username, email, address, phone, website, company }, key)=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$alx$2d$project$2d$0x01$2d$setup$2f$alx$2d$project$2d$0x01$2f$node_modules$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$alx$2d$project$2d$0x01$2d$setup$2f$alx$2d$project$2d$0x01$2f$components$2f$common$2f$UserCard$2e$tsx__$5b$client$5d$__$28$ecmascript$29$__["default"], {
                                     id: id,
                                     name: name,
                                     username: username,
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -31,7 +31,7 @@ const Users: React.FC<UserProps[]> = ({ posts }) => {
             </button>
           </div>
           <div className="grid grid-cols-3 gap-2">
-            {posts?.map(
+            {posts.map(
               (
                 {
                   id,
